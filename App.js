23e9e78dc92995624fdff76f342e26a3ee2e50af
victor Attar <vsla@cin.ufpe.br>
2019-01-24 07:55:24 -0300

    Arrumar erro

--- App.js
+++ App.js
@@ -11,13 +11,12 @@ export default class App extends React.Component {
       projectId: "authentication-2fd09",
       storageBucket: "authentication-2fd09.appspot.com",
       messagingSenderId: "518915834703"
-    }
-    )
+    })
   }
   render() {
     return (
       // The app start calling InitialRoute in routes
-      <InitialRoute persistenceKey={"NavigationState"}/>
+      <InitialRoute/>
     );
    }
 }
