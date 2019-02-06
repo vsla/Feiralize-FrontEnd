4d8ee5a7c531bf640fa374622686588a37da7a09
EduardaVercosa <eduardavercosa@hotmail.com>
2019-02-06 12:47:42 -0300

    testando search bar

--- app/pages/Historico.js
+++ app/pages/Historico.js
@@ -1,13 +1,75 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native';
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  ActivityIndicator,
+  SafeAreaView
+} from "react-native";
+import { List, ListItem, SearchBar } from "react-native-elements";
+import _ from 'lodash'
+import { getUsers, contains } from "../components/SearchHelp";
 
-export default class Historico extends Component {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
 
-    render() {
-        return (
-            <View style={{ flex: 1, alignItems: "center", justifyContent:"center"}}>
-                <Text>Histórico</Text>
-            </View>
-        )
-    }
+    this.state = {
+      loading: false,
+      data: [],
+      error: null,
+      query: "",
+      fullData: [],
+    };
+  }
+
+  componentDidMount() {
+    this.makeRemoteRequest();
+  }
+
+  makeRemoteRequest = () => {
+    this.setState({ loading: true });
+
+    getUsers(20, this.state.query)
+      .then(users => {
+        this.setState({
+          loading: false,
+          data: users,
+          fullData: users,
+        });
+      })
+      .catch(error => {
+        this.setState({ error, loading: false });
+      });
+  };
+
+  handleSearch = text => {
+      const formatQuery = text.toLowerCase();
+      const data = _.filter(this.state.fullData, user => {
+          return contains(user, formatQuery);
+      });
+      this.setState({ query: formatQuery, data }, () => this.makeRemoteRequest());
+  }
+
+  renderHeader = () => {
+    return <SearchBar placeholder="Type Here..." lightTheme round onChangeText={this.handleSearch}/>;
+  };
+
+  render() {
+    return (
+      <SafeAreaView>
+        <FlatList
+          data={this.state.data}
+          renderItem={({ item }) => (
+            <ListItem
+              title={`${item.title}`}
+              containerStyle={{ borderBottomWidth: 0 }}
+            />
+          )}
+          keyExtractor={item => item.email}
+          ListHeaderComponent={this.renderHeader}
+        />
+      </SafeAreaView>
+    );
+  }
 }
