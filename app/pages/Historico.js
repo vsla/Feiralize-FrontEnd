65d90ea63af49e4973b3d7e139bd59dc36b57325
EduardaVercosa <eduardavercosa@hotmail.com>
2019-02-06 22:57:24 -0300

    searchBar

--- app/pages/Historico.js
+++ app/pages/Historico.js
@@ -4,11 +4,13 @@ import {
   Text,
   FlatList,
   ActivityIndicator,
-  SafeAreaView
+  SafeAreaView,
+  TextInput,Image, TouchableHighlight
 } from "react-native";
 import { List, ListItem, SearchBar } from "react-native-elements";
 import _ from 'lodash'
 import { getUsers, contains } from "../components/SearchHelp";
+import ReuseIcon from "../components/ReuseIcon";
 
 export default class App extends Component {
   constructor(props) {
@@ -52,7 +54,30 @@ export default class App extends Component {
   }
 
   renderHeader = () => {
-    return <SearchBar placeholder="Type Here..." lightTheme round onChangeText={this.handleSearch}/>;
+    return (
+      <View style={style.viewStyle}>
+                < View style = {style.headerStyle} >
+                    < TouchableHighlight style={{marginLeft: 20}} onPress={() => this.props.navigation.navigate('Carrinho')}>
+                        <ReuseIcon
+                            name="arrow-back"
+                            color='white'
+                            size={30}
+                        />
+                    </TouchableHighlight>
+                    <View style={style.inputStyle}>
+                        <TextInput style={style.textStyle}
+                            placeholder="Buscar item" onChangeText={this.handleSearch}
+                            placeholderTextColor='white'
+                        />
+                        < ReuseIcon
+                        name = "search"
+                        color = 'white'
+                        size = {30}
+                        />
+                    </View>
+                </View>
+            </View>
+    )
   };
 
   render() {
@@ -66,10 +91,36 @@ export default class App extends Component {
               containerStyle={{ borderBottomWidth: 0 }}
             />
           )}
-          keyExtractor={item => item.email}
+          keyExtractor={item => item.title}
           ListHeaderComponent={this.renderHeader}
         />
       </SafeAreaView>
     );
+  }
+}
+
+const style = {
+  viewStyle: {
+      flex: 1,
+      backgroundColor: 'white',
+  },
+  headerStyle:{
+      flex: 0.18, 
+      flexDirection: "row", 
+      backgroundColor: 'darkorange', 
+      justifyContent: "space-between", 
+      alignItems: 'center',
+  },
+  inputStyle: {
+      flex:2,
+      flexDirection: 'row',
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+      marginRight:20
+  },
+  textStyle: {
+      fontSize: 20,
+      color: 'white',
+      marginRight: 10,
   }
 }
