64776a2cca9a5b96bbc9b78c4df57b0fcc43bdd8
victor Attar <vsla@cin.ufpe.br>
2019-04-18 20:05:22 -0300

    organizando detalhes do pedido

--- app/Supermercado/pages/DetalhesPedido.js
+++ app/Supermercado/pages/DetalhesPedido.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 
 export default class DetalhesPedido extends Component {
   render() {
     return (
-      <View>
-        <Text> textInComponent </Text>
+      <View style={{flex:1,backgroundColor:'#d4d4d4'}}>
+        <View style={style.choice}>
+        </View>
+        <View style={style.container}>
+          <View style={style.containerInfo}>
+            <Text>Juliana Machado</Text>
+            <Text>#Código</Text>
+            <View style={{alignSelf:'stretch'}} />
+            <View style={{flexDirection:'row'}}>
+              <Text>QTD</Text>
+              <Text>Produto</Text>
+              <Text>Valor</Text>
+            </View>
+             <Text>Juliana Machado</Text>
+            <View style={{alignSelf:'stretch'}} />
+            <Text>Juliana Machado</Text>
+            <View style={{alignSelf:'stretch'}} />
+            <Text>Juliana Machado</Text>
+            <View style={{alignSelf:'stretch'}} />
+          </View>
+        </View>
       </View>
     )
   }
 }
+
+const style = StyleSheet.create({
+  choice:{
+    flex:1
+  },
+  container:{
+    flex:10,
+    backgroundColor:'white'
+  },
+  containerInfo:{
+    marginHorizontal:10
+  }
+})
