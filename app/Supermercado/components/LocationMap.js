cdd6f8b6f4ce8c24934b35b9d2eb56b9ad16f20a
victor Attar <vsla@cin.ufpe.br>
2019-04-20 01:51:48 -0300

    Add mapas

--- app/Supermercado/components/LocationMap.js
+++ app/Supermercado/components/LocationMap.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
+import MapView from 'react-native-maps';
+
 
 export default class LocationMap extends Component {
   render() {
     return (
       <View style={{flex:1}}>
-        <Text> Map </Text>
+        <MapView
+          initialRegion={{
+            latitude: -27.2106710,
+            longitude:-49.63622700,
+            latitudeDelta:0.0042,
+            longitudeDelta:0.0031
+          }}
+        style={style.MapView}
+        />
       </View>
     )
   }
 }
+
+const style = StyleSheet.create({
+  MapView:{
+    flex:1,
+  }
+})
