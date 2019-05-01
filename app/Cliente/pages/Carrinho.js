f7d7555c233a71c4220988b388ca119780636b6b
victor Attar <vsla@cin.ufpe.br>
2019-04-30 23:32:50 -0300

    Organizando layout

--- app/Cliente/pages/Carrinho.js
+++ app/Cliente/pages/Carrinho.js
@@ -9,26 +9,25 @@ class Carrrinho extends Component {
     render() {
         return (
             < View style={{flex:1}}>
-                
-                <FlatList
-                    data={this.props.cart}
-                    keyExtractor={item => item.title}
-                    renderItem={( {item} ) =>
-                        <CartCard item={item}/>}
+              <FlatList
+                data={this.props.cart}
+                keyExtractor={item => item.title}
+                renderItem={( {item} ) =>
+                    <CartCard item={item}/>}
+              />
+              <TouchableOpacity
+                onPress={() => this.props.navigation.navigate('ListaProduto') }
+                style={{
+                    position: 'absolute', bottom: 20, right: 25, backgroundColor: '#004000', 
+                    paddingHorizontal:15, borderRadius:50, paddingVertical:5
+                }}
+              >
+                <ReuseIcon
+                name={'add'}
+                size={40}
+                color={'white'}
                 />
-                <TouchableOpacity
-                    onPress={() => this.props.navigation.navigate('ListaProduto') }
-                    style={{
-                        position: 'absolute', bottom: 20, right: 25, backgroundColor: '#004000', 
-                        paddingHorizontal:15, borderRadius:50, paddingVertical:5
-                    }}
-                >
-                    <ReuseIcon
-                    name={'add'}
-                    size={40}
-                    color={'white'}
-                    />
-                </TouchableOpacity>
+              </TouchableOpacity>
             </View >
         )
     };
