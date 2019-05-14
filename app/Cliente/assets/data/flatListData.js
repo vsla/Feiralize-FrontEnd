337314ce32288b30689483e8e8e36434d0bd0e4b
EduardaVercosa <eduardavercosa@hotmail.com>
2019-05-14 14:41:35 -0300

    horarios

---
+++ app/Cliente/assets/data/flatListData.js
@@ -0,0 +1,26 @@
+var flatListData = [
+    {
+        'key': '1',
+        'hora': '18h00 a 18:30'
+    },{
+        'key': '2',
+        'hora': '18h00 a 18:30'
+    },{
+        'key': '3',
+        'hora': '18h00 a 18:30'
+    },{
+        'key': '4',
+        'hora': '18h00 a 18:30'
+    },{
+        'key': '5',
+        'hora': '18h00 a 18:30'
+    },{
+        'key': '6',
+        'hora': '18h00 a 18:30'
+    },{
+        'key': '7',
+        'hora': '18h00 a 18:30'
+    },
+];
+
+module.exports = flatListData;
--- app/Cliente/components/HorarioCard.js
+++ app/Cliente/components/HorarioCard.js
@@ -1,6 +1,19 @@
-import React from 'react';
-import { StyleSheet, Text, View, ScrollView, TouchableOpacity } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
+import flatListData from '../assets/data/flatListData';
 
+class FlatListItem extends Component {
+  render() {
+    return (
+      <View style={{backgroundColor: 'white'}}>
+        <TouchableOpacity>
+          <Text style={styles.textStyle}>{this.props.item.hora}</Text>
+        </TouchableOpacity>
+        <View style={styles.lineStyle}></View>
+      </View>
+      );
+  }
+}
 export default class HorarioCard extends React.Component {
   render() {
     return (
@@ -8,61 +21,23 @@ export default class HorarioCard extends React.Component {
         <View style={{ height: 40, justifyContent: 'center', alignItems: 'flex-start' }}>
           <Text style={{ color: 'grey', fontWeight: 'bold', fontSize: 15, marginLeft: 15 }}>Horários disponíveis</Text>
         </View>
-        <View style={styles.boxStyle}>
-          <ScrollView style={{ width: '100%' }}>
-            <View style={styles.horarioStyle}>
-              <TouchableOpacity
-                onPress={() => { this.props.navigation.navigate('checkout')}}
-              >
-                <Text style={styles.textStyle}>18h00 a 18h30</Text>
-              </TouchableOpacity>
-              <View style={styles.lineStyle}></View>
-              <TouchableOpacity>
-                <View style={{ backgroundColor: 'orange', width: '100%' }}>
-                  <Text style={{
-                    color: 'white',
-                    fontSize: 15,
-                    marginTop: 8,
-                    marginBottom: 8,
-                    paddingLeft: 15,
-                    paddingRight: 15,
-                    paddingTop: 5,
-                    paddingBottom: 5
-                  }}>
-                    18h30 a 19h00
-                  </Text>
-                </View>
-              </TouchableOpacity>
-              <TouchableOpacity>
-                <Text style={styles.textStyle}>19h30 a 20h00</Text>
-              </TouchableOpacity>
-              <View style={styles.lineStyle}></View>
-              <Text style={styles.textStyle}>20h30 a 21h00</Text>
-              <View style={styles.lineStyle}></View>
-              <Text style={styles.textStyle}>21h30 a 22h00</Text>
-            </View>
-          </ScrollView>
-        </View>
+
+        <FlatList
+          data={flatListData}
+          renderItem={({item, index})=>{
+            return (
+              <FlatListItem item={item} index={index}>
+              </FlatListItem>);
+          }}>
+        </FlatList>
+ 
+
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  boxStyle: {
-    width: '100%',
-    height: 200,
-    padding: 3,
-    justifyContent: 'center',
-    alignItems: 'center',
-    flex: 1
-  },
-  horarioStyle: {
-    width: '100%',
-    height: '100%',
-    backgroundColor: 'white',
-
-  },
   textStyle: {
     color: 'grey',
     fontSize: 15,
@@ -75,9 +50,8 @@ const styles = StyleSheet.create({
   },
   lineStyle: {
     height: 1,
-    backgroundColor: 'grey',
+    backgroundColor: 'lightgrey',
     marginLeft: 12,
     marginRight: 12,
   }
 })
-
