2abefbb17e64aaa02906eab4649c948f710e56a4
chiuyong <yongcchiu@gmail.com>
2019-06-03 21:36:32 -0300

    Reducers Integrado. Mostra Erros de Auth. Com bug de firebase initialize

--- App.js
+++ App.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 import { StatusBar } from 'react-native';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'firebase/database';
 import { Provider } from 'react-redux';
-import firebase from 'firebase';
+import store from './app/Cliente/redux';
 import InitialRoute from './app/Cliente/routes/InitialRoute';
+<<<<<<< HEAD
+import Authentication from './app/Cliente/pages/Authentication';
+
+
+=======
 import store from './app/Cliente/redux/index';
+>>>>>>> 0437c45a47d244c57104348692045cd6fb025548
 class App extends Component {
   constructor(props) {
     super(props);
@@ -13,7 +23,24 @@ class App extends Component {
   }
 
   componentWillMount() {
-    if (!firebase.apps.length) {
+    // Initialize firebase instance
+    const firebaseConfig = {
+      apiKey: 'AIzaSyCaWQJ4RcLMEmCvrQShlOIJ3t7dfiLmW3I',
+      authDomain: 'feiralizeapp-a4123.firebaseapp.com',
+      databaseURL: 'https://feiralizeapp-a4123.firebaseio.com',
+      projectId: 'feiralizeapp-a4123',
+      storageBucket: 'feiralizeapp-a4123.appspot.com',
+      messagingSenderId: '314943024760'
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    // Initialize other services on firebase instance
+    // firebase.firestore() // <- needed if using firestore
+    // firebase.functions() // <- needed if using httpsCallable
+    firebase.firestore();
+  }
+    /*if (!firebase.apps.length) {
       firebase.initializeApp({
         apiKey: 'AIzaSyCaWQJ4RcLMEmCvrQShlOIJ3t7dfiLmW3I',
         authDomain: 'feiralizeapp-a4123.firebaseapp.com',
@@ -23,9 +50,14 @@ class App extends Component {
         messagingSenderId: '314943024760'
       });
     } else {
-      firebase.app();
-    } 
-}
+        firebase.app();
+    } */
+
+  //A proposta de fazer authentication com redux
+  //eh para salvar os estados no redux 
+  //ao inves de ser no componente (implementando toda a parte logica)
+  //e o componente ter apenas a função de mostrar e mandar os eventos para o redux
+  //OBS: Call action creator when a user types or clicks a button
 
   render() {
     console.disableYellowBox = true;
